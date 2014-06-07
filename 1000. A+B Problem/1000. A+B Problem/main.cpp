1518236797479a00bd06cfd09b785bb78778f235
Fa[n]tastik <dmitry.koryakin@gmail.com>
2014-06-07 16:37:03 +0300

    Added "1000. A+B Problem"

---
+++ 1000. A+B Problem/1000. A+B Problem/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  1000. A+B Problem
+//
+//  Created by Dmitry Fantastik on 6/7/14.
+//  Copyright (c) 2014 funlife. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a+b;
+    return 0;
+}
+
