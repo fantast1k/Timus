720c624c1d87d50b79c61ccebae6ae2e32d22d60
Fa[n]tastik <dmitry.koryakin@gmail.com>
2015-04-24 22:10:56 +0300

    Add 1293. Эния

---
+++ 1293. Эния/1293. Эния/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  1293. Эния
+//
+//  Created by Dmitry Fantastik on 4/24/15.
+//  Copyright (c) 2015 Fun Life. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    unsigned int N, A, B;
+
+    cin >> N >> A >> B;
+    cout << A * B * N * 2;
+
+    return 0;
+}
