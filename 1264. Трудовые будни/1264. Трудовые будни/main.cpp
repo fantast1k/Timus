d75d97c2afd57552710dbbe240a141c2a46a48ad
Fa[n]tastik <dmitry.koryakin@gmail.com>
2015-04-26 17:11:47 +0300

    Add 1264. Трудовые будни

---
+++ 1264. Трудовые будни/1264. Трудовые будни/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  1264. Трудовые будни
+//
+//  Created by Dmitry Fantastik on 4/26/15.
+//  Copyright (c) 2015 Fun Life. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    unsigned int N, M;
+    cin >> N >> M;
+    cout << (N * ++M);
+    return 0;
+}
